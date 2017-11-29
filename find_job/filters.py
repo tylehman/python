4f02a60b7a30f2a149535b7e85c5cedd2a7bce32
Ty Lehman <tylehman@Tys-MacBook-Pro.local>
2017-11-29 15:37:52 -0700

    1.0.0-SNAPSHOT

--- find_job/filters.py
+++ find_job/filters.py
@@ -3,13 +3,12 @@ from find_job.models import FindJob
 import django_filters
 
 class JobTitleFilter(django_filters.FilterSet):
-    job_title = django_filters.CharFilter(lookup_expr='icontains')
-    comp_name = django_filters.CharFilter(lookup_expr='icontains')
-    job_addr = django_filters.CharFilter(lookup_expr='icontains')
-    job_link = django_filters.CharFilter(lookup_expr='icontains')
-    job_posted = django_filters.DateFilter(name='job_posted')
-    job_search = django_filters.CharFilter(lookup_expr='icontains')
+    job_title = django_filters.CharFilter(lookup_expr='icontains', label='Job Title')
+    comp_name = django_filters.CharFilter(lookup_expr='icontains', label='Company Name')
+    job_addr = django_filters.CharFilter(lookup_expr='icontains', label='Company Location')
+    job_link = django_filters.CharFilter(lookup_expr='icontains', label='Additional Search Info')
+    job_posted = django_filters.DateFilter(name='job_posted', label='Date Posted')
 
     class Meta:
         model = JobInfo
-        fields = ['job_title', 'comp_name', 'job_addr', 'job_link', 'job_posted', 'job_search', ]
+        fields = ['job_title', 'comp_name', 'job_addr', 'job_link', 'job_posted',]
